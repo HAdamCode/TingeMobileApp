08f90a64ef4a4978535cb6c0d11c96e4f6777589
hunteradam <hadam@mines.edu>
2023-03-11 12:49:03 -0700

    ViewModel bare bones created

--- Tinge/app/src/main/java/com/example/tinge/presentation/viewmodel/ITingeViewModel.kt
+++ Tinge/app/src/main/java/com/example/tinge/presentation/viewmodel/ITingeViewModel.kt
@@ -1,4 +1,16 @@
 package com.example.tinge.presentation.viewmodel
 
+import com.example.tinge.data.TingePerson
+import kotlinx.coroutines.flow.StateFlow
+
 interface ITingeViewModel {
+
+    val personListState: StateFlow<List<TingePerson>>
+    val currentPersonState: StateFlow<TingePerson?>
+
+    fun addPerson(characterToAdd: TingePerson)
+
+    fun likePerson(characterToLike: TingePerson)
+
+    fun dislikePerson(characterToDislike: TingePerson)
 }
--- Tinge/app/src/main/java/com/example/tinge/presentation/viewmodel/TingeViewModel.kt
+++ Tinge/app/src/main/java/com/example/tinge/presentation/viewmodel/TingeViewModel.kt
@@ -1,4 +1,95 @@
 package com.example.tinge.presentation.viewmodel
 
-class TingeViewModel {
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.tinge.data.TingePerson
+import com.example.tinge.data.TingeRepo
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
+import java.util.*
+
+class TingeViewModel(private val tingeRepo: TingeRepo) : ViewModel(), ITingeViewModel {
+    companion object {
+        private const val LOG_TAG = "448.TingeViewModel"
+    }
+
+    private val mPersons: MutableList<TingePerson> =
+        mutableListOf()
+
+    /**
+     * holds list of all characters stored within the view model
+     */
+    private val mPersonListState = MutableStateFlow(mPersons.toList())
+
+    override val personListState: StateFlow<List<TingePerson>>
+        get() = mPersonListState.asStateFlow()
+
+    private val mCurrentPersonState: MutableStateFlow<TingePerson?> =
+        MutableStateFlow(null)
+
+    override val currentPersonState: StateFlow<TingePerson?>
+        get() = mCurrentPersonState
+
+    init {
+        Log.d(LOG_TAG, "Characters do be adding")
+        addPerson(tingeRepo.persons.first())
+    }
+
+    /**
+     * Loads a character by id into currentCharacterState, if it exists.  If id is not found
+     * in list of characters, then sets currentCharacterState to null.
+     * @param uuid id to use for character lookup
+     */
+//    override fun loadPersonByUUID(uuid: UUID) {
+//        Log.d(LOG_TAG, "loadCharacterByUUID($uuid)")
+//        mCurrentCharacterState.value = null
+//        mCharacters.forEach { character ->
+//            if (character.id == uuid) {
+//                Log.d(LOG_TAG, "Character found! $character")
+//                mCurrentCharacterState.value = character
+//                return
+//            }
+//        }
+//        Log.d(LOG_TAG, "Character not found")
+//        return
+//    }
+
+    /**
+     * Adds the given character to the list of characters.
+     * @param characterToAdd character to add to the list
+     */
+    override fun addPerson(personToAdd: TingePerson) {
+        Log.d(LOG_TAG, "adding character $personToAdd")
+        mPersonListState.value += personToAdd
+    }
+
+    /**
+     * Deletes corresponding character from the list of characters, if it exists in the list.
+     * Matches characters by id.  If character is not found in the list, does nothing.
+     * @param characterToDelete character to delete from list
+     */
+//    override fun deleteCharacter(characterToDelete: SamodelkinCharacter) {
+//        Log.d(LOG_TAG, "deleting character $characterToDelete")
+//        mCharacters.forEach { character ->
+//            if (character.id == characterToDelete.id) {
+//                mCharacters.remove(character)
+//                if (mCurrentCharacterState.value == character) {
+//                    mCurrentCharacterState.value = null
+//                }
+//                Log.d(LOG_TAG, "character deleted")
+//                return
+//            }
+//        }
+//        Log.d(LOG_TAG, "Character not found")
+//    }
+
+    override fun likePerson(characterToLike: TingePerson) {
+        TODO("Not yet implemented")
+    }
+
+    override fun dislikePerson(characterToDislike: TingePerson) {
+        TODO("Not yet implemented")
+    }
+
 }
