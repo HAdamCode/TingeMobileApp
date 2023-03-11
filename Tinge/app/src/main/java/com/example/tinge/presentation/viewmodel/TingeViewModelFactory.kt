139d372ce6098d75cfe6f793b10420a52a797974
Mike5280 <74518500+Mike5280@users.noreply.github.com>
2023-03-11 13:02:50 -0700

    ViewModelFactory finished ish

--- Tinge/app/src/main/java/com/example/tinge/presentation/viewmodel/TingeViewModelFactory.kt
+++ Tinge/app/src/main/java/com/example/tinge/presentation/viewmodel/TingeViewModelFactory.kt
@@ -1,4 +1,27 @@
 package com.example.tinge.presentation.viewmodel
 
-class TingeViewModelFactory {
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import android.content.Context
+import com.example.tinge.data.TingeRepo
+
+class TingeViewModelFactory: ViewModelProvider.NewInstanceFactory() {
+    companion object {
+        private const val LOG_TAG = "448.TingeViewModelFactory"
+    }
+
+    fun getViewModelClass() = TingeViewModel::class.java
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        Log.d(LOG_TAG, "create() called")
+        if(modelClass.isAssignableFrom(getViewModelClass())) {
+            Log.d(LOG_TAG, "creating ViewModel: ${getViewModelClass()}")
+            return modelClass
+                .getConstructor(TingeViewModel::class.java)
+                .newInstance(TingeRepo)
+        }
+        Log.e(LOG_TAG, "Unknown ViewModel: $modelClass")
+        throw IllegalArgumentException("Unknown ViewModel")
+    }
 }
