e39c327f58108cd33fa211400a46bcae72695f93
hunteradam <hadam@mines.edu>
2023-03-11 15:56:04 -0700

    Profile Edit created

---
+++ Tinge/app/src/main/java/com/example/tinge/presentation/profile/ProfileEditScreen.kt
@@ -0,0 +1,12 @@
+package com.example.tinge.presentation.profile
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.runtime.Composable
+import com.example.tinge.data.TingePerson
+
+@Composable
+fun ProfileEditScreen(person: TingePerson) {
+    Row() {
+
+    }
+}
--- Tinge/app/src/main/java/com/example/tinge/presentation/profile/TingePersonProfile.kt
+++
@@ -1,2 +0,0 @@
-package com.example.tinge.presentation.profile
-
