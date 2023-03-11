6de6f9598044d7c6a04a6b7e318592d803f6ef3a
Mike5280 <74518500+Mike5280@users.noreply.github.com>
2023-03-11 16:02:47 -0700

    Like and dislike buttons created and attached to toasts.

--- Tinge/app/src/main/java/com/example/tinge/presentation/list/TingeListScreen.kt
+++ Tinge/app/src/main/java/com/example/tinge/presentation/list/TingeListScreen.kt
@@ -1,11 +1,17 @@
 package com.example.tinge.presentation.list
 
+import android.content.Context
+import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
-import androidx.compose.material3.Card
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.R
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Call
+import androidx.compose.material.icons.filled.CheckCircle
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
@@ -18,10 +24,13 @@ import kotlin.math.floor
 
 @Composable
 fun TingeListScreen(person: TingePerson) {
-
+    val context = LocalContext.current
     val feet = floor(person.height/12.0).toInt()
     val inches = person.height%12
-    Card(Modifier.fillMaxHeight().fillMaxWidth()) {
+    Card(
+        Modifier
+            .fillMaxHeight()
+            .fillMaxWidth()) {
         Column(Modifier.padding(start = 4.dp, end = 4.dp)) {
                 Text(
                     text = person.name,
@@ -51,11 +60,44 @@ fun TingeListScreen(person: TingePerson) {
                         person.imageId
                     ),
                     contentDescription = "",
-                    Modifier.fillMaxWidth().size(350.dp).fillMaxHeight()
+                    Modifier
+                        .fillMaxWidth()
+                        .size(350.dp)
+                        .fillMaxHeight()
                 )
+            Row(
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .fillMaxWidth()) {
+                IconButton(onClick = { dislikeToast(context) }) {
+                    Icon(
+                        //PLACEHOLDER ICON
+                        imageVector = Icons.Filled.Delete,
+                        contentDescription = "List Desc Placeholder!"
+                    )
+                }
+                IconButton(onClick = { likeToast(context) }) {
+                    Icon(
+                        //PLACEHOLDER ICON
+                        imageVector = Icons.Filled.CheckCircle,
+                        contentDescription = "List Desc Placeholder!"
+                    )
+                }
+
+            }
             }
         }
 
+}
+
+fun likeToast(context: Context) {
+    Toast.makeText(context, "Like button pressed", Toast.LENGTH_SHORT).show()
+
+}
+
+fun dislikeToast(context: Context) {
+    Toast.makeText(context, "Dislike button pressed", Toast.LENGTH_SHORT).show()
 }
 
 @Preview
