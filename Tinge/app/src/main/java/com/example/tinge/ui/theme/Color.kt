d79943438a78988fcbacddbb431bc382bba53b4c
Lucas Kellem <lucaskellem@mines.edu>
2023-03-11 17:32:52 -0700

    Colors

--- Tinge/app/src/main/java/com/example/tinge/ui/theme/Color.kt
+++ Tinge/app/src/main/java/com/example/tinge/ui/theme/Color.kt
@@ -1,5 +1,6 @@
 package com.example.tinge.ui.theme
 
+import android.graphics.Color.parseColor
 import androidx.compose.ui.graphics.Color
 
 val Purple80 = Color(0xFFD0BCFF)
@@ -9,3 +10,7 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val Orange1 = Color(parseColor("#ff8e32"))
+//val Orange2
+val Pink1 = Color(0xFFFF99FF)
